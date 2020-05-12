28d258d8bc7b5c9fa2fbf982ac889713d42139b9
Eunice <eunicecompra@gmail.com>
2020-05-12 22:36:53 +1000

    Implemented left command

---
+++ src/main/scala/com/toyrobot/LeftCommand.scala
@@ -0,0 +1,16 @@
+package com.toyrobot
+
+import com.toyrobot.model.Table
+
+case class LeftCommand()
+
+object LeftCommand {
+  def execute(table: Table): Table = {
+    table.direction match {
+      case "NORTH" => table.copy(direction = "WEST")
+      case "WEST" => table.copy(direction = "SOUTH")
+      case "SOUTH" => table.copy(direction = "EAST")
+      case "EAST" => table.copy(direction = "NORTH")
+    }
+  }
+}
