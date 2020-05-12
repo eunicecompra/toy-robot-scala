0b9db4053e41a24bec3b2c0856f2fa311ae0c570
Eunice <eunicecompra@gmail.com>
2020-05-12 22:03:01 +1000

    Made Place as a case class for command pattern matching

--- src/main/scala/com/toyrobot/Place.scala
+++ src/main/scala/com/toyrobot/Place.scala
@@ -2,6 +2,8 @@ package com.toyrobot
 
 import com.toyrobot.model.Table
 
+case class Place()
+
 object Place {
   def execute(table: Table, command: String): Table = {
     val location: Array[String] = command.replaceFirst("PLACE ", "").split(',')
