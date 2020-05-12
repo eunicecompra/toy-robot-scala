0493e3d5230c36334e210f8176c9547da9b0479e
Eunice <eunicecompra@gmail.com>
2020-05-12 21:53:52 +1000

    Use of table model in place command

--- src/main/scala/com/toyrobot/Place.scala
+++ src/main/scala/com/toyrobot/Place.scala
@@ -1,8 +1,11 @@
 package com.toyrobot
 
+import com.toyrobot.model.Table
+
 object Place {
-  def execute(command: String): String = {
-    command.replaceFirst("PLACE ", "")
+  def execute(table: Table, command: String): Table = {
+    val location: Array[String] = command.replaceFirst("PLACE ", "").split(',')
+    table.copy(location(0).toInt, location(1).toInt, location(2))
   }
 
 }
--- src/test/scala/com/toyrobot/PlaceSpec.scala
+++ src/test/scala/com/toyrobot/PlaceSpec.scala
@@ -1,12 +1,13 @@
 package com.toyrobot
 
+import com.toyrobot.model.Table
 import org.specs2.mutable.Specification
 
 class PlaceSpec extends Specification {
   "Place command" >> {
     "where toy robot's location is 0,0,NORTH" >> {
-      val currentLocation = Place.execute("PLACE 0,0,NORTH")
-      currentLocation === "0,0,NORTH"
+      val table = Place.execute(Table(-1, -1, null), "PLACE 0,0,NORTH")
+      table ==== Table(0, 0, "NORTH")
     }
   }
 
